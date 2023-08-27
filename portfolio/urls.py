fb17dcf48a2e4bb28808373acff92402141ab7ed
KhayitovDev <khayitovkhayitboy2109@gmail.com>
2023-08-27 11:26:42 +0500

    changed bootstrap to tailwind

---
+++ portfolio/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import HomePageListView
+urlpatterns = [
+    path('', HomePageListView.as_view(), name='home_page')
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, TemplateView
 # Create your views here.
+
+class HomePageListView(TemplateView):
+    template_name='index.html'
+
